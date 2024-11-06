aafc97974f22baf182d12636a33ef1914638efb0
Tiisetso-Polori <118134452+Tiisetso-Polori@users.noreply.github.com>
2024-11-06 16:17:38 +0200

    my clone's changes.

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/', (req,res)=>{
+
+  
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
